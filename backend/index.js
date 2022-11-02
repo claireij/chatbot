bd010c7857e425c324c03ef6f4ccfc8cdd72a66b
Claire <ruppelclaire@yahoo.de>
2022-11-02 20:23:59 +0100

    Build connection between frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io');
 
 const port = process.env.PORT || 4001;
 const app = express();
 const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        // TODO hardcodierung auflösen
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on('chat message', (message) => {
+        // TODO backend logic
+        console.log("message" + message);
+        io.emit('chat message', message)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+      });
+})
+
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 import MessageList from './components/MessageList';
 import {BiCalculator} from 'react-icons/bi';
+import socketIOClient from "socket.io-client";
+
+import {useState} from 'react';
+
+const ENDPOINT = "http://127.0.0.1:4001";
+
+//TODO prüfen ob es einen besseren Weg gibt, das einzubinden
+const newSocket = socketIOClient(ENDPOINT);
+    newSocket.on('connect', (socket) => {
+      console.log('socket connected', socket);
+    })
 
 function App() {
-  const messageList = [
-    {
+
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState("");
+
+  // const messageList = [
+  //   {
+  //     sender: "bot",
+  //     message: "some message",
+  //     date: "24.02.2031 14:03"
+  //   },
+  //   {
+  //     sender: "user",
+  //     message: "some other message",
+  //     date: "03.02.2001 14:00"
+  //   }
+  // ]
+
+  newSocket.on('chat message', function(msg) {
+    const newMessageList = [...messageList, {
+      message: msg,
       sender: "bot",
-      message: "some message",
-      date: "24.02.2031 14:03"
-    },
-    {
+      //TODO change date to date from the database
+      date: Date.now()
+    }]
+    setMessageList(newMessageList);
+  })
+
+  const handleInputChange = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const handleMessageSend = (e) => {
+    e.preventDefault();
+    newSocket.emit('chat message', message);
+    const newMessageList = [...messageList, {
+      message: message,
       sender: "user",
-      message: "some other message",
-      date: "03.02.2001 14:00"
-    }
-  ]
+      date: Date.now()
+    }]
+    setMessageList(newMessageList);
+  }
 
   return (
     <div className="div--chat">
@@ -25,8 +64,14 @@ function App() {
           <MessageList messageList={messageList} />
         </div>
         <div className="div--input">
-          <input type="text"></input>
-          <button>
+          <input 
+          type="text"
+          value={message}
+          onChange={handleInputChange}
+          ></input>
+          <button
+          onClick={handleMessageSend}
+          >
             <BiCalculator className="button--icon" />
             Calculate me now!
           </button>
