8910ee6d0df777290086329deb55142de2ab36ce
Claire <ruppelclaire@yahoo.de>
2022-11-04 22:22:54 +0100

    Added input check for letters

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const Chat = require('./models/ChatSchema');
 
 const port = process.env.PORT || 4001;
 const app = express();
